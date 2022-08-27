d0017a2a9b3e4dba42744442d567ad0031c4d1e0
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-08-27 17:30:37 -0400

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//router
+import IndexRouters from "./router/index"
+
+//scss
+import "./assets/scss/socialv.scss"
+import "./assets/scss/customizer.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <IndexRouters />
+    </div>
+  );
+}
+
+export default App;
